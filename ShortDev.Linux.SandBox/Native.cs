bae8ce93be65c5132a566f9fe0ffbc230f6ee23d
Lukas Kurz <lukas@shortdev.de>
2023-04-20 18:56:42 +0200

    Fix seccomp enum values

--- ShortDev.Linux.SandBox/Native.cs
+++ ShortDev.Linux.SandBox/Native.cs
@@ -4,9 +4,6 @@ namespace ShortDev.Linux.SandBox;
 
 internal static unsafe class Native
 {
-    [DllImport("libc")]
-    public static extern int seccomp(SECCOMP operation, SECCOMP_FILTER_FLAG flags, void* args);
-
     [DllImport("libc", SetLastError = true)]
     public static extern int prctl(PR option, ulong arg2, ulong arg3, ulong arg4, ulong arg5);
 
@@ -16,26 +13,15 @@ internal static unsafe class Native
 
 internal enum PR : int
 {
-    SET_SECCOMP = 22
+    GET_SECCOMP = 21,
+    SET_SECCOMP,
+    SET_NO_NEW_PRIVS = 38,
+    GET_NO_NEW_PRIVS
 }
 
 internal enum SECCOMP : uint
 {
-    SET_MODE_STRICT,
-    SET_MODE_FILTER,
-    GET_ACTION_AVAIL,
-    GET_NOTIF_SIZES
-}
-
-internal enum SECCOMP_FILTER_FLAG : uint
-{
-    TSYNC = 1 << 0,
-    LOG = 1 << 1,
-    SPEC_ALLOW = 1 << 2,
-    NEW_LISTENER = 1 << 3,
-    TSYNC_ESRCH = 1 << 4,
-    /// <summary>
-    /// Received notifications wait in killable state (only respond to fatal signals)
-    /// </summary>
-    WAIT_KILLABLE_RECV = 1 << 5
+    MODE_DISABLED,
+    MODE_STRICT,
+    MODE_FILTER
 }
--- ShortDev.Linux.SandBox/SandBox.cs
+++ ShortDev.Linux.SandBox/SandBox.cs
@@ -7,8 +7,12 @@ public sealed class SandBox
 {
     public static void EnableStrict()
     {
+        // https://github.com/dropbox/lepton/blob/f34c7f42d2e0d688239cb880648dc152713c4eae/src/io/Seccomp.cc#L118
+        if (Native.prctl(PR.SET_NO_NEW_PRIVS, 1, 0, 0, 0) != 0)
+            throw new UnixException();
+
         // https://github.com/microsoft/WSL/issues/4035
-        if (Native.prctl(PR.SET_SECCOMP, (ulong)SECCOMP.SET_MODE_STRICT, 0, 0, 0) == -1)
+        if (Native.prctl(PR.SET_SECCOMP, (ulong)SECCOMP.MODE_STRICT, 0, 0, 0) != 0)
             throw new UnixException();
     }
 }
